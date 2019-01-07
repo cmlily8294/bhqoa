53530673c499e432d2f1f236ad3721c6753bb504
cmlily8294 <cmlily8294@gmail.com>
2019-01-08 00:06:46 +0800

    feature:登录页面

---
+++ lib/pages/login/view/login.dart
@@ -0,0 +1,20 @@
+import 'package:bhqoa/common/route/base_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends BaseState<LoginPage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+    );
+  }
+}
